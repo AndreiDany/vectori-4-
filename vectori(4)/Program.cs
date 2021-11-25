1a21c860eb57c9b4332ababcd2e345b4c8ec1c5f
Daniel Leon Andrei <daniel.andrei@emmanuel.ro>
2021-11-25 17:45:06 +0200

    Hello

---
+++ vectori(4)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace vectori_4_
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[] fibo = Fibonacci(5);
+            Console.WriteLine(fibo[0]);
+            for (int i = 0; i < fibo.Length; i++)
+            {
+                if (fibo[i] != 0)
+                Console.WriteLine(fibo[i]);
+            }
+        }
+        
+
+        static int[] Fibonacci(int n)
+        {
+            int[] F = new int[100];
+            F[0] = 0;
+            if(n==0) return F;
+            F[1] = 1;
+            if (n == 1) return F;
+            int i = 1;
+           
+            while (F[i] < n)
+            {
+                i++;
+                F[i] = F[i - 1] + F[i - 2];
+
+            }
+            return F;
+        }
+    }
+}
